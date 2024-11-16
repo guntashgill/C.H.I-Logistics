d5be526786ec18e3398c47dbbda6865514e8a641
Guntash Gill <gillguntash@yahoo.com>
2024-11-16 11:53:25 -0800

    new updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
-import { Link } from 'react-router-dom'; // Import Link for routing
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-transparent position-absolute w-100">
-      <div className="container d-flex justify-content-center align-items-center">
-        <a className="navbar-brand text-white me-5" href="/">
-          <img src="/logo.png" alt="Logo" className="img-fluid" style={{ maxHeight: '50px' }} /> {/* Add your logo here */}
+      <div className="container d-flex justify-content-between align-items-center">
+        <a className="navbar-brand text-white" href="/">
+          <img src="/logo.png" alt="Logo" className="img-fluid" style={{ maxHeight: '50px' }} />
         </a>
         <div className="d-flex align-items-center">
           <a href="https://www.instagram.com" className="text-white mx-2">
@@ -18,22 +23,38 @@ const Header = () => {
             <FaFacebook size={24} />
           </a>
         </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <div
+          className={`collapse navbar-collapse ${
+            isNavbarOpen ? 'show custom-navbar' : ''
+          } justify-content-end`} // Opens to the right
+        >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link text-white" href="#about">About Us</a> {/* Link to About Us section */}
+              <a className="nav-link text-white" href="#about">
+                About Us
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#services">Services</a> {/* Link to Services section */}
+              <a className="nav-link text-white" href="#services">
+                Services
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#contact">Contact Us</a> {/* Link to Contact Us section */}
+              <a className="nav-link text-white" href="#contact">
+                Contact Us
+              </a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/apply">Drive For C.H.I</Link> {/* Link to Apply page */}
+              <Link className="nav-link text-white" to="/apply">
+                Drive For C.H.I
+              </Link>
             </li>
           </ul>
         </div>
